36f944dfbe80c59300610f791d8c8095e9c0417b
Kumar Devendra <67205183+KrumarDevendra007@users.noreply.github.com>
2025-05-22 13:05:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstChar(text) {
   // your code here
+	const result = text.trim();
+	if(result.length > 0){
+		console.log(result.substring(0,1))
+	}
+	else{
+		console.log('')
+	}
+	
 }
 
 // Do not change the code below
